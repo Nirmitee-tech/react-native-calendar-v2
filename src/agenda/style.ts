069f209c60d841c272d523b95ff6522a7ef378e4
Abhay Kumar <careers.abhaykumar@gmail.com>
2025-10-21 20:24:57 +0530

    first commit

--- src/agenda/style.ts
+++ src/agenda/style.ts
@@ -1,57 +1,63 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as defaultStyle from '../style';
 import platformStyles from './platform-style';
-import {Theme} from '../types';
+import { Theme } from '../types';
 
-export default function styleConstructor(theme: Theme = {}) {
-  const appStyle = {...defaultStyle, ...theme};
-  const {knob, weekdays} = platformStyles(appStyle);
+const RESERVATIONS_MARGIN_TOP = 104;
 
-  return StyleSheet.create({
-    container: {
-      flex: 1,
-      overflow: 'hidden'
-    },
-    animatedContainer: {
-      flex: 1
-    },
-    knob,
-    weekdays,
-    header: {
-      overflow: 'hidden',
-      justifyContent: 'flex-end',
-      position: 'absolute',
-      height: '100%',
-      width: '100%'
-    },
-    knobContainer: {
-      flex: 1,
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      height: 24,
-      bottom: 0,
-      alignItems: 'center',
-      backgroundColor: appStyle.calendarBackground
-    },
-    dayHeader: {
-      width: 32,
-      textAlign: 'center',
-      fontSize: appStyle.textDayHeaderFontSize,
-      fontFamily: appStyle.textDayHeaderFontFamily,
-      fontWeight: appStyle.textDayHeaderFontWeight,
-      color: appStyle.textSectionTitleColor
-    },
-    reservations: {
-      flex: 1,
-      marginTop: 104,
-      backgroundColor: appStyle.reservationsBackgroundColor || appStyle.backgroundColor //TODO: remove 2nd in V2
-    },
-    scrollPadStyle: {
-      position: 'absolute',
-      width: '100%',
-      alignSelf: 'center'
-    },
-    ...(theme['stylesheet.agenda.main'] || {})
-  });
+/**
+ * Creates styles for the Agenda component
+ * @param theme - Optional theme object to override default styles
+ * @returns StyleSheet object for the Agenda component
+ */
+export default function styleConstructor(theme: Partial<Theme> = {}): StyleSheet.NamedStyles<any> {
+    const appStyle = { ...defaultStyle, ...theme };
+    const { knob, weekdays } = platformStyles(appStyle);
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            overflow: 'hidden'
+        },
+        animatedContainer: {
+            flex: 1
+        },
+        knob,
+        weekdays,
+        header: {
+            overflow: 'hidden',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            height: '100%',
+            width: '100%'
+        },
+        knobContainer: {
+            flex: 1,
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            height: 24,
+            bottom: 0,
+            alignItems: 'center',
+            backgroundColor: appStyle.calendarBackground
+        },
+        dayHeader: {
+            width: 32,
+            textAlign: 'center',
+            fontSize: appStyle.textDayHeaderFontSize,
+            fontFamily: appStyle.textDayHeaderFontFamily,
+            fontWeight: appStyle.textDayHeaderFontWeight,
+            color: appStyle.textSectionTitleColor
+        },
+        reservations: {
+            flex: 1,
+            marginTop: RESERVATIONS_MARGIN_TOP,
+            backgroundColor: appStyle.reservationsBackgroundColor || appStyle.backgroundColor //TODO: remove 2nd in V2
+        },
+        scrollPadStyle: {
+            position: 'absolute',
+            width: '100%',
+            alignSelf: 'center'
+        },
+        ...(theme['stylesheet.agenda.main'] || {})
+    });
 }
